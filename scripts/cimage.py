0d286eb7ff28b4caf1e3fdee90177f7fdf633f04
TaAnh <taanh42@gmail.com>
2023-09-11 16:46:58 +0700

    fix conflict

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -2,9 +2,9 @@ import tempfile
 from ifnude import detect
 
 def convert_to_sd(img):
-    shapes = []
-    chunks = detect(img)
-    for chunk in chunks:
-        shapes.append(True)
-        # shapes.append(chunk["score"] > 0.7)
-    return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
+    # shapes = []
+    # chunks = detect(img)
+    # for chunk in chunks:
+    #     shapes.append(True)
+    #     # shapes.append(chunk["score"] > 0.7)
+    return [False, tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
